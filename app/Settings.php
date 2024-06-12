f4f13360ad8cd98213d59e0a6dbf5aa49ab8ca51
Moustafa Gouda <bafi99@gmail.com>
2024-06-12 21:26:55 +0300

    Add plugin notification to continue setup on plugins page

--- app/Settings.php
+++ app/Settings.php
@@ -24,11 +24,10 @@ use const PCC_PLUGIN_DIR_URL;
 class Settings
 {
 	private $pages = [
-		'disconnect-collection' => PCC_PLUGIN_DIR . 'admin/templates/partials/disconnect-collection.php',
-		'confirm-disconnect' => PCC_PLUGIN_DIR . 'admin/templates/partials/confirm-disconnect.php',
 		'authenticate' => PCC_PLUGIN_DIR . 'admin/templates/partials/authenticate.php',
-		'connected-collection'  => PCC_PLUGIN_DIR . 'admin/templates/partials/disconnect-collection.php',
+		'connected-collection'  => PCC_PLUGIN_DIR . 'admin/templates/partials/connected-collection.php',
 		'create-collection' => PCC_PLUGIN_DIR . 'admin/templates/partials/create-collection.php',
+		'disconnect-confirmation'  => PCC_PLUGIN_DIR . 'admin/templates/partials/disconnect-confirmation.php',
 		'setup' => PCC_PLUGIN_DIR . 'admin/templates/partials/setup.php',
 	];
 
@@ -49,6 +48,7 @@ class Settings
 			'admin_enqueue_scripts',
 			[$this, 'enqueueAssets']
 		);
+		add_action('admin_menu', [$this, 'pluginAdminNotice']);
 	}
 
 	/**
@@ -77,12 +77,16 @@ class Settings
 	public function renderSettingsPage(): void
 	{
 		$view = isset($_GET['view']) ? $_GET['view'] : null;
-		if ($view) {
+		if ($view && isset($this->pages[$view])) {
 			require $this->pages[$view];
 			return;
 		}
 
-		if ($this->getCredentials()) {
+		// Site id is set and Credentials are set
+		if ($this->getSiteId() && $this->getCredentials()) {
+			require $this->pages['connected-collection'];
+		// Credentials is set but Site id is not set then user needs to create a new site
+		} elseif ($this->getCredentials()) {
 			require $this->pages['create-collection'];
 		} else {
 			require $this->pages['setup'];
@@ -118,6 +122,7 @@ class Settings
 				'rest_url' => get_rest_url(get_current_blog_id(), PCC_API_NAMESPACE),
 				'nonce' => wp_create_nonce('wp_rest'),
 				'plugin_main_page' => menu_page_url(PCC_HANDLE, false),
+				'site_url' => site_url(),
 			] + ['credentials' => $this->getCredentials()]
 		);
 	}
@@ -132,5 +137,37 @@ class Settings
 		$pccCredentials = get_option(PCC_CREDENTIALS_OPTION_KEY);
 
 		return $pccCredentials ? unserialize($pccCredentials) : [];
+	}
+
+	/**
+	 * @return false|mixed|null
+	 */
+	private function getSiteId()
+	{
+		return get_option(PCC_SITE_ID_OPTION_KEY);
+	}
+
+	/**
+	 * Show notification when authentication details are not set or collection not created
+	 */
+	public function pluginAdminNotice()
+	{
+		global $pagenow;
+		if ($pagenow != 'plugins.php') {
+			return;
+		}
+
+		// Show notification when authentication details are not set or collection not created
+		if (!$this->getCredentials() || !$this->getSiteId()) {
+			add_action('admin_notices', [$this, 'pluginNotification']);
+		}
+	}
+
+	/**
+	 * Plugin notification to continue setup
+	 */
+	public function pluginNotification()
+	{
+		require PCC_PLUGIN_DIR . 'admin/templates/partials/plugin-notification.php';
 	}
 }
