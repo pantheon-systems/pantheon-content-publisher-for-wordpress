60e8d416292864a2882c21e5e49f583cff29a01f
Moustafa Gouda <bafi99@gmail.com>
2024-07-09 15:31:27 +0300

    Revert "Bugfix: Redirect to WP JSON endpoint"
    This reverts commit b9a0dcb474479fcba9724206173ec9f41edcaaff.

--- app/Settings.php
+++ app/Settings.php
@@ -106,9 +106,7 @@ class Settings
 		global $wp;
 		$strLen = strlen(static::PCC_PUBLISH_DOCUMENT_ENDPOINT);
 		if (substr($wp->request, 0, $strLen) !== static::PCC_PUBLISH_DOCUMENT_ENDPOINT) {
-			$url = rest_url(PCC_API_NAMESPACE . '/' . static::PCC_STATUS_ENDPOINT);
-
-			return wp_redirect($url);
+			return;
 		}
 
 		// Publish document
