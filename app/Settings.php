655f824e32b96ff3ea925d05347d06fdc83f2783
Al N <anitu@crowdfavorite.com>
2024-07-26 12:03:52 +0300

    cleanup Settings class

--- app/Settings.php
+++ app/Settings.php
@@ -31,24 +31,29 @@ class Settings
 	 * Pantheon menu icon in base64
 	 */
 	// phpcs:ignore Generic.Files.LineLength.TooLong
-	private const PCC_ICON_BASE64 = 'PHN2ZyB3aWR0aD0iMTYiIGhlaWdodD0iMTYiIHZpZXdCb3g9IjAgMCAxNiAxNiIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KICAgIDxwYXRoIGQ9Ik00LjcxNjkxIDFMNi4xNTA3MSA0LjQ1NDE4SDQuMzI1ODdMNC45MTI0MiA1Ljk1MzE2SDguNjI3MjlMNC43MTY5MSAxWiIgZmlsbD0id2hpdGUiLz4KICAgIDxwYXRoIGQ9Ik05LjU3MjI5IDEzLjU0NThMOC45NTMxNCAxMi4wNDY5SDguMTA1ODlMNi4zNDYyMiA3Ljc3ODAySDUuNTk2NzNMNy4zNTY0IDEyLjA0NjlINS4yMDU2OUw5LjE4MTI1IDE3TDcuNzQ3NDQgMTMuNTQ1OEg5LjU3MjI5WiIKICAgICAgICAgIGZpbGw9IndoaXRlIi8+CiAgICA8cGF0aCBkPSJNMTAuMDYxMSAxMC41MTUzSDcuNzQ3NDRMOC4yMzYyNCAxMS42ODg0SDEwLjA2MTFDMTAuMDkzNyAxMS42ODg0IDEwLjIyNCAxMS42MjMyIDEwLjIyNCAxMS4xMDE4QzEwLjE5MTQgMTAuNTgwNCAxMC4wOTM3IDEwLjUxNTMgMTAuMDYxMSAxMC41MTUzWiIKICAgICAgICAgIGZpbGw9IndoaXRlIi8+CiAgICA8cGF0aCBkPSJNMTAuMjg5MiA5LjExNDA0SDcuMTkzNDhMNy42ODIyOCAxMC4yODcySDEwLjI4OTJDMTAuMzIxOCAxMC4yODcyIDEwLjQ1MjEgMTAuMjIyIDEwLjQ1MjEgOS43MDA2QzEwLjQxOTYgOS4xNzkyMiAxMC4zMjE4IDkuMTE0MDQgMTAuMjg5MiA5LjExNDA0WiIKICAgICAgICAgIGZpbGw9IndoaXRlIi8+CiAgICA8cGF0aCBkPSJNMTAuMDYxMSA3LjQ4NDczQzEwLjA5MzcgNy40ODQ3MyAxMC4yMjQgNy40MTk1NiAxMC4yMjQgNi44OTgxN0MxMC4yMjQgNi4zNzY3OSAxMC4xMjYzIDYuMzExNjEgMTAuMDYxMSA2LjMxMTYxSDcuNTE5MzVMOC4wMDgxNSA3LjQ4NDczSDEwLjA2MTFaIgogICAgICAgICAgZmlsbD0id2hpdGUiLz4KICAgIDxwYXRoIGQ9Ik04LjU2MjEgOC44ODU5NUgxMC4yNTY2QzEwLjI4OTIgOC44ODU5NSAxMC40MTk1IDguODIwNzcgMTAuNDE5NSA4LjI5OTM5QzEwLjQxOTUgNy43NzggMTAuMzIxOCA3LjcxMjgzIDEwLjI1NjYgNy43MTI4M0g4LjA3MzNMOC41NjIxIDguODg1OTVaIgogICAgICAgICAgZmlsbD0id2hpdGUiLz4KICAgIDxwYXRoIGQ9Ik01Ljc1OTY3IDguODg1OTVMNS4yMDU3IDcuNDg0NzNINi40NzY1OEw3LjA2MzE0IDguODg1OTVIOC4yNjg4NEw3LjE5MzQ4IDYuMzExNjFINC41NTM5N0M0LjM1ODQ1IDYuMzExNjEgNC4yMjgxMSA2LjMxMTYxIDQuMTMwMzUgNi42MDQ4OUM0LjAzMjU5IDYuOTYzMzUgNCA3LjY0NzY2IDQgOC45ODM3MUM0IDEwLjMxOTggNCAxMS4wMDQxIDQuMTMwMzUgMTEuMzYyNUM0LjIyODExIDExLjY1NTggNC4zMjU4NyAxMS42NTU4IDQuNTUzOTcgMTEuNjU1OEg2Ljg2NzYyTDUuNzU5NjcgOC44ODU5NVoiCiAgICAgICAgICBmaWxsPSJ3aGl0ZSIvPgo8L3N2Zz4=';
+	private const string PCC_ICON_BASE64 = 'PHN2ZyB3aWR0aD0iMTYiIGhlaWdodD0iMTYiIHZpZXdCb3g9IjAgMCAxNiAxNiIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KICAgIDxwYXRoIGQ9Ik00LjcxNjkxIDFMNi4xNTA3MSA0LjQ1NDE4SDQuMzI1ODdMNC45MTI0MiA1Ljk1MzE2SDguNjI3MjlMNC43MTY5MSAxWiIgZmlsbD0id2hpdGUiLz4KICAgIDxwYXRoIGQ9Ik05LjU3MjI5IDEzLjU0NThMOC45NTMxNCAxMi4wNDY5SDguMTA1ODlMNi4zNDYyMiA3Ljc3ODAySDUuNTk2NzNMNy4zNTY0IDEyLjA0NjlINS4yMDU2OUw5LjE4MTI1IDE3TDcuNzQ3NDQgMTMuNTQ1OEg5LjU3MjI5WiIKICAgICAgICAgIGZpbGw9IndoaXRlIi8+CiAgICA8cGF0aCBkPSJNMTAuMDYxMSAxMC41MTUzSDcuNzQ3NDRMOC4yMzYyNCAxMS42ODg0SDEwLjA2MTFDMTAuMDkzNyAxMS42ODg0IDEwLjIyNCAxMS42MjMyIDEwLjIyNCAxMS4xMDE4QzEwLjE5MTQgMTAuNTgwNCAxMC4wOTM3IDEwLjUxNTMgMTAuMDYxMSAxMC41MTUzWiIKICAgICAgICAgIGZpbGw9IndoaXRlIi8+CiAgICA8cGF0aCBkPSJNMTAuMjg5MiA5LjExNDA0SDcuMTkzNDhMNy42ODIyOCAxMC4yODcySDEwLjI4OTJDMTAuMzIxOCAxMC4yODcyIDEwLjQ1MjEgMTAuMjIyIDEwLjQ1MjEgOS43MDA2QzEwLjQxOTYgOS4xNzkyMiAxMC4zMjE4IDkuMTE0MDQgMTAuMjg5MiA5LjExNDA0WiIKICAgICAgICAgIGZpbGw9IndoaXRlIi8+CiAgICA8cGF0aCBkPSJNMTAuMDYxMSA3LjQ4NDczQzEwLjA5MzcgNy40ODQ3MyAxMC4yMjQgNy40MTk1NiAxMC4yMjQgNi44OTgxN0MxMC4yMjQgNi4zNzY3OSAxMC4xMjYzIDYuMzExNjEgMTAuMDYxMSA2LjMxMTYxSDcuNTE5MzVMOC4wMDgxNSA3LjQ4NDczSDEwLjA2MTFaIgogICAgICAgICAgZmlsbD0id2hpdGUiLz4KICAgIDxwYXRoIGQ9Ik04LjU2MjEgOC44ODU5NUgxMC4yNTY2QzEwLjI4OTIgOC44ODU5NSAxMC40MTk1IDguODIwNzcgMTAuNDE5NSA4LjI5OTM5QzEwLjQxOTUgNy43NzggMTAuMzIxOCA3LjcxMjgzIDEwLjI1NjYgNy43MTI4M0g4LjA3MzNMOC41NjIxIDguODg1OTVaIgogICAgICAgICAgZmlsbD0id2hpdGUiLz4KICAgIDxwYXRoIGQ9Ik01Ljc1OTY3IDguODg1OTVMNS4yMDU3IDcuNDg0NzNINi40NzY1OEw3LjA2MzE0IDguODg1OTVIOC4yNjg4NEw3LjE5MzQ4IDYuMzExNjFINC41NTM5N0M0LjM1ODQ1IDYuMzExNjEgNC4yMjgxMSA2LjMxMTYxIDQuMTMwMzUgNi42MDQ4OUM0LjAzMjU5IDYuOTYzMzUgNCA3LjY0NzY2IDQgOC45ODM3MUM0IDEwLjMxOTggNCAxMS4wMDQxIDQuMTMwMzUgMTEuMzYyNUM0LjIyODExIDExLjY1NTggNC4zMjU4NyAxMS42NTU4IDQuNTUzOTcgMTEuNjU1OEg2Ljg2NzYyTDUuNzU5NjcgOC44ODU5NVoiCiAgICAgICAgICBmaWxsPSJ3aGl0ZSIvPgo8L3N2Zz4=';
 
 	/**
 	 * Pantheon Cloud Status endpoint required by PCC
 	 */
-	private const PCC_STATUS_ENDPOINT = 'api/pantheoncloud/status';
+	private const string PCC_STATUS_ENDPOINT = 'api/pantheoncloud/status';
 
 	/**
 	 * Publish document endpoint required by PCC
 	 */
-	private const PCC_PUBLISH_DOCUMENT_ENDPOINT = 'api/pantheoncloud/document/';
+	private const string PCC_PUBLISH_DOCUMENT_ENDPOINT = 'api/pantheoncloud/document/';
 
 	/**
 	 * Google Docs edit URL.
 	 */
-	private const PCC_DOCUMENT_EDIT_URL = 'https://docs.google.com/document/d/%s/edit';
+	private const string PCC_DOCUMENT_EDIT_URL = 'https://docs.google.com/document/d/%s/edit';
 
-	private $pages = [
+	/**
+	 * List of pages to render.
+	 *
+	 * @var array
+	 */
+	private array $pages = [
 		'connected-collection' => PCC_PLUGIN_DIR . 'admin/templates/partials/connected-collection.php',
 		'create-collection' => PCC_PLUGIN_DIR . 'admin/templates/partials/create-collection.php',
 		'disconnect-confirmation' => PCC_PLUGIN_DIR . 'admin/templates/partials/disconnect-confirmation.php',
@@ -89,7 +94,7 @@ class Settings
 	/**
 	 * Publish documents from Google Docs.
 	 */
-	public function publishDocuments()
+	public function publishDocuments(): void
 	{
 		global $wp;
 		if (!str_starts_with($wp->request, static::PCC_PUBLISH_DOCUMENT_ENDPOINT)) {
@@ -111,8 +116,9 @@ class Settings
 		// Preview document
 		if (
 			// phpcs:ignore WordPress.Security.NonceVerification.Recommended
-			isset($_GET['pccGrant']) && isset($_GET['publishingLevel']) &&
-			PublishingLevel::REALTIME->value === $_GET['publishingLevel'] // phpcs:ignore WordPress.Security.NonceVerification.Recommended
+			isset($_GET['pccGrant'], $_GET['publishingLevel']) &&
+			// phpcs:ignore WordPress.Security.NonceVerification.Recommended
+			PublishingLevel::REALTIME->value === $_GET['publishingLevel']
 		) {
 			$parts = explode('/', $wp->request);
 			$documentId = end($parts);
@@ -133,14 +139,17 @@ class Settings
 	 * Register custom endpoint for Pantheon Cloud Status.
 	 * This endpoint is used to check if the site is hosted live.
 	 * and checked only one time to show your website on PCC google addon
+	 *
+	 * @return void
 	 */
-	public function registerPantheonCloudStatusEndpoint()
+	public function registerPantheonCloudStatusEndpoint(): void
 	{
 		global $wp;
 		if (static::PCC_STATUS_ENDPOINT === $wp->request) {
 			$url = rest_url(PCC_API_NAMESPACE . '/' . static::PCC_STATUS_ENDPOINT);
 
-			return wp_redirect($url);
+			wp_redirect($url);
+			exit();
 		}
 	}
 
@@ -154,7 +163,7 @@ class Settings
 		global $pagenow;
 		// Check if the current page is the post/page edit page
 		// phpcs:ignore WordPress.Security.NonceVerification.Recommended
-		if ($pagenow == 'post.php' && isset($_GET['post']) && 'edit' === strtolower($_GET['action'])) {
+		if ($pagenow === 'post.php' && isset($_GET['post']) && 'edit' === strtolower($_GET['action'])) {
 			// phpcs:ignore
 			$documentId = get_post_meta(intval($_GET['post']), PCC_CONTENT_META_KEY, true);
 			if (!$documentId) {
@@ -367,9 +376,13 @@ class Settings
 	{
 		if (
 			// phpcs:ignore WordPress.Security.NonceVerification.Recommended
-			isset($_GET['preview']) && $_GET['preview'] === 'google_document' && isset($_GET['document_id'])
-			&& $_GET['document_id'] && isset($_GET['publishing_level']) && // phpcs:ignore WordPress.Security.NonceVerification.Recommended
-			$_GET['publishing_level'] === PublishingLevel::REALTIME->value // phpcs:ignore WordPress.Security.NonceVerification.Recommended
+			isset($_GET['preview'], $_GET['document_id'], $_GET['publishing_level']) &&
+			// phpcs:ignore WordPress.Security.NonceVerification.Recommended
+			$_GET['preview'] === 'google_document' &&
+			// phpcs:ignore WordPress.Security.NonceVerification.Recommended
+			$_GET['document_id'] &&
+			// phpcs:ignore WordPress.Security.NonceVerification.Recommended
+			$_GET['publishing_level'] === PublishingLevel::REALTIME->value
 		) {
 			wp_enqueue_script(
 				PCC_HANDLE,
@@ -394,8 +407,10 @@ class Settings
 
 	/**
 	 * Show notification when authentication details are not set or collection not created
+	 *
+	 * @return void
 	 */
-	public function pluginAdminNotice()
+	public function pluginAdminNotice(): void
 	{
 		global $pagenow;
 		if ($pagenow !== 'plugins.php') {
@@ -410,8 +425,10 @@ class Settings
 
 	/**
 	 * Plugin notification to continue setup
+	 *
+	 * @return void
 	 */
-	public function pluginNotification()
+	public function pluginNotification(): void
 	{
 		require PCC_PLUGIN_DIR . 'admin/templates/partials/plugin-notification.php';
 	}
@@ -423,7 +440,7 @@ class Settings
 	 *
 	 * @return string The custom list table class.
 	 */
-	public function overrideAdminWPPostsTable($className)
+	public function overrideAdminWPPostsTable(string $className): string
 	{
 		if ('WP_Posts_List_Table' === $className) {
 			return PccPostsListTable::class;
